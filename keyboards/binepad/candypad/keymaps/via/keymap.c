2c3e63d8789f4d373ac1bedea91ca5cfe3b5c3af
Vino Rodrigues <366673+vinorodrigues@users.noreply.github.com>
2023-10-15 18:19:51 +1100

    Swap rotary encoder

--- keyboards/binepad/candypad/keymaps/via/keymap.c
+++ keyboards/binepad/candypad/keymaps/via/keymap.c
@@ -10,7 +10,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_P4,   KC_P5,   KC_P6,
         KC_P1,   KC_P2,   KC_P3,   KC_PENT,
         KC_P0,            KC_PDOT,
-        KC_MUTE, KC_MPLY
+        KC_MPLY, KC_MUTE
     )
 };
 
@@ -18,8 +18,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [0] = {
-        ENCODER_CCW_CW(KC_VOLD, KC_VOLU),
-        ENCODER_CCW_CW(KC_WH_U, KC_WH_D)
+        ENCODER_CCW_CW(KC_WH_U, KC_WH_D),
+        ENCODER_CCW_CW(KC_VOLD, KC_VOLU)
     }
 };
 
