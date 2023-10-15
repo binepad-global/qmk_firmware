72524b0940baee84b9ade819aba5d51dc48a5179
Vino Rodrigues <366673+vinorodrigues@users.noreply.github.com>
2023-10-15 18:00:56 +1100

    Corrected keymap

--- keyboards/binepad/candypad/keymaps/via/keymap.c
+++ keyboards/binepad/candypad/keymaps/via/keymap.c
@@ -6,11 +6,11 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS,
-        KC_P7,   KC_P8,   KC_P9,
-        KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
-        KC_P1,   KC_P2,   KC_P3,
-        KC_P0,            KC_PDOT, KC_PENT,
-        KC_MUTE
+        KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
+        KC_P4,   KC_P5,   KC_P6,
+        KC_P1,   KC_P2,   KC_P3,   KC_PENT,
+        KC_P0,            KC_PDOT,
+        KC_MUTE, KC_MPLY
     )
 };
 
@@ -18,7 +18,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [0] = {
-        ENCODER_CCW_CW(KC_VOLD, KC_VOLU)
+        ENCODER_CCW_CW(KC_VOLD, KC_VOLU),
+        ENCODER_CCW_CW(KC_WH_U, KC_WH_D)
     }
 };
 
