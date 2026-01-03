4141fd93667e8eddbec45a87da2766c54d4a107a
silvinor <366673+silvinor@users.noreply.github.com>
2026-01-03 20:13:58 +1100

    reinstate i2c default sets to see if this fixes the no-oled thing

--- keyboards/binepad/candypad/config.h
+++ keyboards/binepad/candypad/config.h
@@ -6,12 +6,17 @@
 /* ----- Encoder ----- */
 #define ENCODER_DEFAULT_POS 0x3 // enable 1:1 resolution
 
-/* ----- I2C ----- */
-#define I2C1_CLOCK_SPEED 400000
-#define I2C1_DUTY_CYCLE FAST_DUTY_CYCLE_2
+// /* ----- I2C ----- */
+// #define I2C1_CLOCK_SPEED 400000
+// #define I2C1_DUTY_CYCLE FAST_DUTY_CYCLE_2
 
 /* ----- OLED ----- */
 #ifdef OLED_ENABLE
+/* I2C (for OLED) */
+#    define I2C1_SCL_PIN B6
+#    define I2C1_SDA_PIN B7
+#    define I2C_DRIVER I2CD1
+
 /* Configure oled driver for the 128x32 oled */
 #    define OLED_TIMEOUT (2 * 60 * 1000) // 2 minutes
 #    define OLED_UPDATE_INTERVAL 33       // ~30fps
